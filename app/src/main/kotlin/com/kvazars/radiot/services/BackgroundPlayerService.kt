230da7f9ccce00f194360d5c3d3d624af15f696a
Leonid Zavodnik <bboy.kvazar@gmail.com>
2018-05-26 14:54:50 +0200

    Add stream notification stop action

--- app/src/main/kotlin/com/kvazars/radiot/services/BackgroundPlayerService.kt
+++ app/src/main/kotlin/com/kvazars/radiot/services/BackgroundPlayerService.kt
@@ -4,6 +4,7 @@ import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
@@ -44,27 +45,27 @@ class BackgroundPlayerService : Service() {
 
         val appComponent = RadioTApplication.getAppComponent(this)
         appComponent
-            .newsInteractor()
-            .activeNews
-            .retryWhen { appComponent.reconnectTrigger() }
-            .subscribe(
-                { updateActiveNewsNotification(it) },
-                { it.printStackTrace() }
-            )
-            .addTo(disposableBag)
+                .newsInteractor()
+                .activeNews
+                .retryWhen { appComponent.reconnectTrigger() }
+                .subscribe(
+                        { updateActiveNewsNotification(it) },
+                        { it.printStackTrace() }
+                )
+                .addTo(disposableBag)
 
         appComponent
-            .streamPlayer()
-            .statusUpdates
-            .distinctUntilChanged()
-            .filter { it == PodcastStreamPlayer.Status.STOPPED || it == PodcastStreamPlayer.Status.ERROR }
-            .subscribe(
-                {
-                    stopForeground(true)
-                    stopSelf()
-                }
-            )
-            .addTo(disposableBag)
+                .streamPlayer()
+                .statusUpdates
+                .distinctUntilChanged()
+                .filter { it == PodcastStreamPlayer.Status.STOPPED || it == PodcastStreamPlayer.Status.ERROR }
+                .subscribe(
+                        {
+                            stopForeground(true)
+                            stopSelf()
+                        }
+                )
+                .addTo(disposableBag)
     }
 
     override fun onDestroy() {
@@ -80,19 +81,24 @@ class BackgroundPlayerService : Service() {
 
     private fun createNotification(news: Optional<NewsItem>): Notification {
         val builder = NotificationCompat.Builder(this, NotificationUtils.NotificationChannelInfo.PRIMARY.id)
-            .setSmallIcon(R.drawable.ic_audiotrack)
-            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .setAutoCancel(false)
-            .setOngoing(true)
-            .setContentIntent(createContentIntent())
+                .setSmallIcon(R.drawable.ic_audiotrack)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .addAction(
+                        0,
+                        getString(R.string.notification_action_stop),
+                        NotificationIntentReceiver.getStreamStopIntent(this)
+                )
+                .setContentIntent(createContentIntent())
 
         val newsItem = news.value
         if (newsItem != null) {
             builder.setContentTitle(newsItem.title)
             builder.setContentText(newsItem.snippet)
             builder.setStyle(
-                NotificationCompat.BigTextStyle()
-                    .bigText(newsItem.snippet)
+                    NotificationCompat.BigTextStyle()
+                            .bigText(newsItem.snippet)
             )
         } else {
             builder.setContentTitle(getString(R.string.stream_no_active_news))
@@ -109,6 +115,24 @@ class BackgroundPlayerService : Service() {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
         return PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
+    class NotificationIntentReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+           RadioTApplication.getAppComponent(context).streamPlayer().stop()
+        }
+
+        companion object {
+            fun getStreamStopIntent(context: Context): PendingIntent? {
+                return PendingIntent.getBroadcast(
+                        context,
+                        0,
+                        Intent(context, NotificationIntentReceiver::class.java),
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            }
+        }
+
     }
 
 }
